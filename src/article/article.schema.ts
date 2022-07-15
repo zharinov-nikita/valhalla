5afcf36491eaab767fc572b21703af110d4d1d2a
Nikita <zharinov.nicita@gmail.com>
2022-07-15 13:44:42 +0700

    feature/article dto

--- src/article/article.schema.ts
+++ src/article/article.schema.ts
@@ -1,12 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import mongoose, { Document, ObjectId } from 'mongoose'
 
 export type ArticleDocument = Article & Document
 
 @Schema()
 export class Article {
+  @Prop({ type: mongoose.Types.ObjectId, ref: 'Repository', required: true })
+  repositoryId: ObjectId
+
   @Prop({ type: String, required: true, unique: false })
   title: string
+
+  @Prop({ type: String, required: true, unique: false })
+  description: string
+
+  @Prop({ type: String, required: true, unique: false })
+  body: string
 }
 
 export const ArticleSchema = SchemaFactory.createForClass(Article)
--- src/article/dto/update-article.dto.ts
+++ src/article/dto/update-article.dto.ts
@@ -1,3 +1,6 @@
 export class UpdateArticleDto {
+  readonly repositoryId: string
   readonly title: string
+  readonly description: string
+  readonly body: string
 }
