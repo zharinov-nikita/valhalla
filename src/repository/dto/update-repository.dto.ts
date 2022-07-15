6cca937d28701af6b3592fd35209a26caa0939cc
Nikita <zharinov.nicita@gmail.com>
2022-07-15 14:32:49 +0700

    featur/user fix RepositorySchema

--- src/repository/dto/update-repository.dto.ts
+++ src/repository/dto/update-repository.dto.ts
@@ -1,3 +1,6 @@
+import { ObjectId } from 'mongoose'
+
 export class UpdateRepositoryDto {
+  readonly userId: ObjectId
   readonly title: string
 }
--- src/repository/repository.schema.ts
+++ src/repository/repository.schema.ts
@@ -1,10 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import mongoose, { Document, ObjectId } from 'mongoose'
 
 export type RepositoryDocument = Repository & Document
 
 @Schema()
 export class Repository {
+  @Prop({ type: mongoose.Types.ObjectId, ref: 'Repository', required: true })
+  userId: ObjectId
+
   @Prop({ type: String, required: true, unique: false })
   title: string
 }
