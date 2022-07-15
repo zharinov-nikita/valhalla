6fd4eb5ed095b9a2b2ae1df379c24174277fb1d1
Nikita <zharinov.nicita@gmail.com>
2022-07-15 14:26:42 +0700

    feature/user UpdateSchema

---
+++ src/user/user.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+
+export type UserDocument = User & Document
+
+@Schema()
+export class User {
+  @Prop({ type: String, required: true, unique: true })
+  login: string
+
+  @Prop({ type: String, required: true, unique: false })
+  password: string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
