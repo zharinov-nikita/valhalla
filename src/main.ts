70b1b7566292363459e3b9c4951ceffa15cdeea7
Nikita <zharinov.nicita@gmail.com>
2022-07-15 11:54:18 +0700

    feature/api-key try catch server start

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-  console.log(`Server start port ${process.env.PORT}`);
+async function start() {
+  try {
+    const app = await NestFactory.create(AppModule)
+    app.enableCors()
+    await app.listen(process.env.PORT)
+    console.log(`Server start ${process.env.PORT}`)
+  } catch (e) {
+    console.log(e)
+  }
 }
-bootstrap();
+start()
