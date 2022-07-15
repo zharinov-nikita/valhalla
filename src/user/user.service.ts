1d0bb5f649778950aa1d5038e415a5b16c91f9f9
Nikita <zharinov.nicita@gmail.com>
2022-07-15 14:27:55 +0700

    feature/user UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,38 +1,34 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model, ObjectId } from 'mongoose'
-import { Article, ArticleDocument } from './article.schema'
-import { CreateArticleDto } from './user/create-user.dto'
-import { FindArticleDto } from './user/find-article.dto'
-import { UpdateArticleDto } from './user/update-user.dto'
+import { User, UserDocument } from './user.schema'
+import { CreateUserDto } from './user/create-user.dto'
+import { UpdateUserDto } from './user/update-user.dto'
 
 @Injectable()
-export class ArticleService {
+export class UserService {
   constructor(
-    @InjectModel(Article.name)
-    private articleModel: Model<ArticleDocument>
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>
   ) {}
 
-  async create(dto: CreateArticleDto): Promise<Article> {
-    return await this.articleModel.create(dto)
+  async create(dto: CreateUserDto): Promise<User> {
+    return await this.userModel.create(dto)
   }
 
-  async find(dto: FindArticleDto): Promise<Article[]> {
-    return await this.articleModel.find(dto)
+  async find(): Promise<User[]> {
+    return await this.userModel.find()
   }
 
-  async findById(_id: ObjectId): Promise<Article> {
-    return await this.articleModel.findById({ _id })
+  async findById(_id: ObjectId): Promise<User> {
+    return await this.userModel.findById({ _id })
   }
 
-  async findByIdAndUpdate(
-    _id: ObjectId,
-    dto: UpdateArticleDto
-  ): Promise<Article> {
-    return await this.articleModel.findByIdAndUpdate(_id, dto, { new: true })
+  async findByIdAndUpdate(_id: ObjectId, dto: UpdateUserDto): Promise<User> {
+    return await this.userModel.findByIdAndUpdate(_id, dto, { new: true })
   }
 
-  async findByIdAndDelete(_id: ObjectId): Promise<Article> {
-    return await this.articleModel.findByIdAndDelete({ _id })
+  async findByIdAndDelete(_id: ObjectId): Promise<User> {
+    return await this.userModel.findByIdAndDelete({ _id })
   }
 }
